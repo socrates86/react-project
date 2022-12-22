b179bcd75280cfabe1d795d08046b57f1874f7bf
socrates86 <morrisonikechukwu86@gmail.com>
2022-12-22 16:11:20 +0100

    project-2

---
+++ src/components.js/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return(
+        <div>
+            <nav className="nav-1">
+             <img src="./logo512.png" alt="logo" width="70px" className="nav--icon" />
+             <h3 className="nav--logo_text">ReactFact</h3>
+             <h4 className="nav--title">React Course-Project-1</h4>
+             </nav>
+        </div>
+    )
+}
