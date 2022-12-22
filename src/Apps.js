74ba320f2014a73aa2af7b88e050a7dd87f7903a
socrates86 <morrisonikechukwu86@gmail.com>
2022-12-22 15:23:33 +0100

    made some changes

---
+++ src/Apps.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components.js/Navbar";
+import Main from "./components.js/Main";
+
+
+
+
+export default function Apps() {
+    return(
+        <div>
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
